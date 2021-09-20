10cc1c4176b4490c2d642f6a261d7c8ed86ac1b4
Haruna Ishikawa <n2.m5.1f@gmail.com>
2021-09-20 10:08:26 +0900

    js修正

--- _static/dist/assets/js/animation.js
+++ _static/dist/assets/js/animation.js
@@ -1,26 +1,26 @@
 //loading animetion
 window.onload = function () {
     const loader = document.getElementById('loading');
-    loader.classList.add('p-loading_completed');
+    loader.classList.add('p-loading__completed');
 }
 
 //animation
 $(function () {
     $(window).scroll(function () {
-        $('.js_animation').each(function () {
+        $('.js-animation').each(function () {
             var element = $(this).offset().top;
             var scroll = $(window).scrollTop();
             var windowHeight = $(window).height();
             if (scroll > element - windowHeight + 100) {
-                $(this).addClass('js_scrollIn');
+                $(this).addClass('js-scrollIn');
             }
         });
 
         //clip-pathを使用したテキスト表示
-        // $('.js_animation.js_scrollIn').each(function() {
-        //     var currentText = $(this).find('.js_animation_clipPath_text');
-        //     if (currentText.hasClass('js_animation_trigger')) {
-        //         currentText.addClass('js_animation_trigger_fadeOut');
+        // $('.js-animation.js-scrollIn').each(function() {
+        //     var currentText = $(this).find('.js-animation__clipPath__text');
+        //     if (currentText.hasClass('js-animation__trigger')) {
+        //         currentText.addClass('js-animation__trigger_fadeOut');
         //     }
         // });
     });
--- _static/dist/assets/js/common.js
+++ _static/dist/assets/js/common.js
@@ -4,10 +4,19 @@
 -----------------------------
 */
 (function($) {
+  /*　- スムーズスクロール -
+  /*-----------------------------------*/
+  $('a[href^="#"]').click(function () {
+    var href = $(this).attr("href");
+    var target = $(href);
+    var position = target.offset().top;
+    $('body,html').stop().animate({ scrollTop: position }, 500);
+  });
+
   /* - ページトップボタン
   /* ---------------------------------*/
   function pagetopButton() {
-    var $pagetop      = $('.pagetop-button');
+    var $pagetop      = $('.p-pagetop');
     var positionValue = $(window).scrollTop();
     var showPosition  = 300;
     var easing        = 'swing';
@@ -46,7 +55,7 @@
   /* - ハンバーガーナビ
   /* ---------------------------------*/
   $('.p-hamburger').on('click', function () {
-    $('body').toggleClass('is-drawerActive');
+    $('.c-globalnavigation__area').toggleClass('is-active');
     $('.p-hamburger_text').toggleClass('is-active');
     if ($(this).attr('aria-expanded') == 'false') {
       $(this).attr('aria-expanded', true);
@@ -55,28 +64,28 @@
     }
   });
 
-  /* - スマホメニュー閉じるボタンの処理 -ハンバーガーナビと連動-
+  /* - スマホメニュー閉じる処理 -ハンバーガーナビと連動-
   /* ---------------------------------------------------------*/
-  var $triggerBtn = $('.js-close_trigger');
+  var $triggerBtn = $('.js-open__trigger');
   $triggerBtn.on('click', function (event) {
     $('.p-hamburger').trigger('click');
   });
 
   /*　スマホ　アコーディオンメニュー
   /* ---------------------------------*/
-  $(window).on('load resize', function () {
-    var winW = $(window).width();
-    var devW = 767;
-    var $spDropdownMenu = $('.js-dropdownmenu_open_trigger');
-    if (winW <= devW) {
-      $spDropdownMenu.on('click', function () {
-        if ($(this).next('.globalnav_dropdownmenu_sp').hasClass('is-active')) {
-          $('.globalnav_dropdownmenu_sp').removeClass('is-active');
-        } else {
-          $('.globalnav_dropdownmenu_sp').addClass('is-active');
-        }
-        return false;
-      });
-    }
-  });
+  // $(window).on('load resize', function () {
+  //   var winW = $(window).width();
+  //   var devW = 767;
+  //   var $spDropdownMenu = $('.js-open__trigger');
+  //   if (winW <= devW) {
+  //     $spDropdownMenu.on('click', function () {
+  //       if ($(this).next('.globalnav_dropdownmenu_sp').hasClass('is-active')) {
+  //         $('.globalnav_dropdownmenu_sp').removeClass('is-active');
+  //       } else {
+  //         $('.globalnav_dropdownmenu_sp').addClass('is-active');
+  //       }
+  //       return false;
+  //     });
+  //   }
+  // });
 }(jQuery));
